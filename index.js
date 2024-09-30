9f8a6f1d1fbfca0d4be3a8a969ee14c0856355e5
robertay99 <robertay99@hotmail.com>
2024-09-29 21:09:33 -0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan"; // Change "Joe" to "Susan" to pass the first test
+const height = 30; // Change 74 to a value less than 40 to pass the second test
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
-console.log(message);
+module.exports = { name, height, message };
+console.log(message); // This will log "Susan is 30 inches tall"
